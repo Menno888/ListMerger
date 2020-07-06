6ac98c800b400ed33913618aec92532a9ba9360c
mennovanrijsselberg <menno@hireserve.nl>
2020-07-06 17:20:27 +0200

    Moved filtering to separate class

---
+++ src/main/java/Filter.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+public class Filter {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void filter(SongList songList, String lists) {
+        String[] toKeep = lists.split(",");
+        List<String> toKeepList = Arrays.asList(toKeep);
+        ArrayList<String> allEditions = songList.tagCheckup();
+        List<String> newList = new ArrayList<>();
+        for (String edition : allEditions) {
+            if (toKeepList.contains(edition.substring(0, edition.length() - 4)) || toKeepList.contains(edition)) {
+                newList.add(edition);
+            }
+        }
+        String shouldAppearInAll = "n";
+        String retainUnused = "n";
+        System.out.println("Should appear [y] or not [n] appear in specified lists?");
+        String shouldAppear = sc.nextLine();
+        if(newList.size() > 1 && "y".equals(shouldAppear)) {
+            System.out.println("Should appear in all lists or not? [y/n]");
+            shouldAppearInAll = sc.nextLine();
+        }
+        if("y".equals(shouldAppear)) {
+            System.out.println("Should retain lists that aren't in the selection?");
+            retainUnused = sc.nextLine();
+        }
+        if("y".equals(shouldAppear) && "n".equals(shouldAppearInAll)) {
+            for(Record currentRecord : songList) {
+                boolean containsEntries = false;
+                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
+                    if (newList.contains(entry2.getKey())) {
+                        containsEntries = true;
+                        break;
+                    }
+                }
+                if(!containsEntries) {
+                    currentRecord.cleanPositionMap();
+                }
+            }
+            songList.normalize();
+            allEditions.removeAll(newList);
+            if("n".equals(retainUnused)) {
+                for(Record currentRecord : songList) {
+                    for(String edition : allEditions) {
+                        currentRecord.getPositionMap().remove(edition);
+                    }
+                }
+            }
+        }
+        else if("y".equals(shouldAppear) && "y".equals(shouldAppearInAll)) {
+            for(Record currentRecord : songList) {
+                int containsEntries = 0;
+                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
+                    if (newList.contains(entry2.getKey())) {
+                        containsEntries++;
+                    }
+                }
+                if(containsEntries != newList.size()) {
+                    currentRecord.cleanPositionMap();
+                }
+            }
+            songList.normalize();
+            allEditions.removeAll(newList);
+            if("n".equals(retainUnused)) {
+                for(Record currentRecord : songList) {
+                    for(String edition : allEditions) {
+                        currentRecord.getPositionMap().remove(edition);
+                    }
+                }
+            }
+        }
+        else {
+            for(Record currentRecord : songList) {
+                boolean containsEntries = false;
+                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
+                    if (newList.contains(entry2.getKey())) {
+                        containsEntries = true;
+                        break;
+                    }
+                }
+                if(containsEntries) {
+                    currentRecord.cleanPositionMap();
+                }
+            }
+            songList.normalize();
+        }
+    }
+}
--- src/main/java/StartMerging.java
+++ src/main/java/StartMerging.java
@@ -20,7 +20,7 @@ public class StartMerging {
                 case "f":
                     System.out.println("Type lists or a set of list");
                     String toKeep = sc.nextLine();
-                    filter(toKeep);
+                    Filter.filter(songList, toKeep);
                     break;
                 case "m":
                     System.out.println("Enter a file to initialize/merge:");
@@ -67,90 +67,6 @@ public class StartMerging {
                     System.out.println("Invalid input, try again");
                     break;
             }
-        }
-    }
-
-    private static void filter(String lists) {
-        String[] toKeep = lists.split(",");
-        List<String> toKeepList = Arrays.asList(toKeep);
-        ArrayList<String> allEditions = songList.tagCheckup();
-        List<String> newList = new ArrayList<>();
-        for (String edition : allEditions) {
-            if (toKeepList.contains(edition.substring(0, edition.length() - 4)) || toKeepList.contains(edition)) {
-                newList.add(edition);
-            }
-        }
-        String shouldAppearInAll = "n";
-        String retainUnused = "n";
-        System.out.println("Should appear [y] or not [n] appear in specified lists?");
-        String shouldAppear = sc.nextLine();
-        if(newList.size() > 1 && "y".equals(shouldAppear)) {
-            System.out.println("Should appear in all lists or not? [y/n]");
-            shouldAppearInAll = sc.nextLine();
-        }
-        if("y".equals(shouldAppear)) {
-            System.out.println("Should retain lists that aren't in the selection?");
-            retainUnused = sc.nextLine();
-        }
-        if("y".equals(shouldAppear) && "n".equals(shouldAppearInAll)) {
-            for(Record currentRecord : songList) {
-                boolean containsEntries = false;
-                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
-                    if (newList.contains(entry2.getKey())) {
-                        containsEntries = true;
-                        break;
-                    }
-                }
-                if(!containsEntries) {
-                    currentRecord.cleanPositionMap();
-                }
-            }
-            songList.normalize();
-            allEditions.removeAll(newList);
-            if("n".equals(retainUnused)) {
-                for(Record currentRecord : songList) {
-                    for(String edition : allEditions) {
-                        currentRecord.getPositionMap().remove(edition);
-                    }
-                }
-            }
-        }
-        else if("y".equals(shouldAppear) && "y".equals(shouldAppearInAll)) {
-            for(Record currentRecord : songList) {
-                int containsEntries = 0;
-                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
-                    if (newList.contains(entry2.getKey())) {
-                        containsEntries++;
-                    }
-                }
-                if(containsEntries != newList.size()) {
-                    currentRecord.cleanPositionMap();
-                }
-            }
-            songList.normalize();
-            allEditions.removeAll(newList);
-            if("n".equals(retainUnused)) {
-                for(Record currentRecord : songList) {
-                    for(String edition : allEditions) {
-                        currentRecord.getPositionMap().remove(edition);
-                    }
-                }
-            }
-        }
-        else {
-            for(Record currentRecord : songList) {
-                boolean containsEntries = false;
-                for (Map.Entry<String, Integer> entry2 : currentRecord.getPositionMap().entrySet()) {
-                    if (newList.contains(entry2.getKey())) {
-                        containsEntries = true;
-                        break;
-                    }
-                }
-                if(containsEntries) {
-                    currentRecord.cleanPositionMap();
-                }
-            }
-            songList.normalize();
         }
     }
 }
