cc5093a772a88e52234480574852fd857ae0dabb
Menno van Rijsselberg <menno@ubeeo.nl>
2022-09-28 17:26:14 +0200

    Added extra ToplijstenMergerTools module (global climbs/drops)

--- src/main/java/ToplijstenMergerTools.java
+++ src/main/java/ToplijstenMergerTools.java
@@ -10,7 +10,7 @@ public class ToplijstenMergerTools {
     public static void getTools(SongList list) {
         boolean takeInput = true;
         while(takeInput) {
-            System.out.println("Tools: (h)ighest (p)ositions, (w)ithout (p)ositions, count (re)entries, (p)oint (l)ist, (q)uit");
+            System.out.println("Tools: (h)ighest (p)ositions, (w)ithout (p)ositions, count (re)entries, (p)oint (l)ist, (q)uit, (y)early (e)xtremes");
             String control = sc.nextLine();
             if ("hp".equals(control)) {
                 SongList newList = new SongList();
@@ -97,10 +97,70 @@ public class ToplijstenMergerTools {
             else if ("q".equals(control)) {
                 takeInput = false;
             }
+            else if ("ye".equals(control)) {
+                SongList newList = new SongList();
+                System.out.println("Enter two year numbers separated by a comma for which you want to generate the diff list (Warning! Can take ~10m)");
+                String yearString = sc.nextLine();
+                String[] yearStringArray = yearString.split(",");
+                int yearTo = Math.max(Integer.parseInt(yearStringArray[0]), Integer.parseInt(yearStringArray[1]));
+                int yearFrom = Math.min(Integer.parseInt(yearStringArray[0]), Integer.parseInt(yearStringArray[1]));
+                Set<String> allListsAbbreviations = getAllListAbbreviations(list);
+                for (Record r : list) {
+                    Record copy = new Record(r.getArtist(), r.getTitle());
+                    if (inAnyListThisOrPreviousYear(r, yearTo, yearFrom)) {
+                        double averageClimb = getAverageClimbForRecordComparedToYear(r, allListsAbbreviations, yearTo, yearFrom, list);
+                        copy.addPositionToMap("Percentage", (int) averageClimb);
+                        double remainder = (averageClimb % 1) * 1000;
+                        copy.addPositionToMap("Remainder", (int) remainder);
+                        System.out.println("ADD - SongName: " + r.showSong());
+                    } else {
+                        copy.addPositionToMap("Percentage", 0);
+                        copy.addPositionToMap("Remainder", 0);
+                        System.out.println("SKIP - SongName: " + r.showSong());
+                    }
+                    newList.add(copy);
+                }
+                newList.outputToFile();
+                System.out.println("Wrote current SongList calculated with average climb/drop and remainder");
+            }
             else {
                 System.out.println("Invalid input, try again");
             }
         }
+    }
+
+    private static double getAverageClimbForRecordComparedToYear(Record record, Set<String> allListAbbreviations, int yearTo, int yearFrom, SongList list) {
+        List<Double> climbsAndDrops = new ArrayList<>();
+        for (String abbr : allListAbbreviations) {
+            String abbrFirstPart = abbr.substring(0, abbr.length() - 4);
+            String abbrSecondPart = abbr.substring(abbr.length() - 4);
+            if (String.valueOf(yearTo).equals(abbrSecondPart) && allListAbbreviations.contains(abbrFirstPart + yearFrom)) {
+
+                String currentYear = abbrFirstPart + yearTo;
+                String prevYear = abbrFirstPart + yearFrom;
+                int currentYearLength = getListLength(currentYear, list);
+                int prevYearLength = getListLength(prevYear, list);
+
+                HashMap<String, Integer> positionMap = record.getPositionMap();
+                double climbOrDrop;
+                if (positionMap.containsKey(currentYear) && positionMap.containsKey(prevYear)) {
+                    climbOrDrop = ((double) positionMap.get(prevYear) - (double) positionMap.get(currentYear)) / (double) Math.max(currentYearLength + 1, prevYearLength + 1);
+                } else if (positionMap.containsKey(currentYear) && !positionMap.containsKey(prevYear)) {
+                    climbOrDrop = ((double) prevYearLength + 1 - (double) positionMap.get(currentYear)) / (double) Math.max(currentYearLength + 1, prevYearLength + 1);
+                } else if (!positionMap.containsKey(currentYear) && positionMap.containsKey(prevYear)) {
+                    climbOrDrop = ((double) positionMap.get(prevYear) - (double) currentYearLength + 1) / (double) Math.max(currentYearLength + 1, prevYearLength + 1);
+                } else {
+                    climbOrDrop = 0.0;
+                }
+                climbsAndDrops.add(climbOrDrop * 100);
+            }
+        }
+
+        return climbsAndDrops
+                .stream()
+                .mapToDouble(e -> e)
+                .average()
+                .orElse(0.0);
     }
 
     private static int getListLength(String key, SongList list) {
@@ -114,5 +174,26 @@ public class ToplijstenMergerTools {
             }
         }
         return currentMax;
+    }
+
+    private static Set<String> getAllListAbbreviations(SongList list) {
+        Set<String> lists = new HashSet<>();
+        for (Record r : list) {
+            lists.addAll(r.getPositionMap().keySet());
+        }
+        return lists;
+    }
+
+    private static boolean inAnyListThisOrPreviousYear(Record record, int yearTo, int yearFrom) {
+        LinkedHashMap<String, Integer> positionMap = record.getPositionMap();
+        boolean wasInAList = false;
+        for (String key : positionMap.keySet()) {
+            String listAbbrYearPart = key.substring(key.length() - 4);
+            if (String.valueOf(yearTo).equals(listAbbrYearPart) || String.valueOf(yearFrom).equals(listAbbrYearPart)) {
+                wasInAList = true;
+                break;
+            }
+        }
+        return wasInAList;
     }
 }
