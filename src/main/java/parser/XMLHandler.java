e8887642d7d8497b97a7690f6f053af5db08f0b1
Menno van Rijsselberg <menno@ubeeo.nl>
2024-03-28 17:14:12 +0100

    Added separate handler for the XML Parser

---
+++ src/main/java/parser/XMLHandler.java
@@ -0,0 +1,75 @@
+package parser;
+
+import dto.Song;
+import dto.SongList;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XMLHandler extends DefaultHandler {
+
+    private static final String COLUMN_HEADER_RECORD = "record";
+    private static final String COLUMN_HEADER_ARTIST = "Artiest";
+    private static final String COLUMN_HEADER_SONG = "Nummer";
+    private static final String INFO_COLUMN_MARKER = "ADD-";
+
+    private static final String SEPARATOR_CHARACTER_AMPERSAND = "&";
+    private static final String SEPARATOR_CHARACTER_AMPERSAND_XML_SAFE = "&amp;";
+
+    private final StringBuilder xml = new StringBuilder();
+    private boolean building = false;
+    private final SongList songList = new SongList();
+    private String elementTag;
+
+    public SongList getSongList() {
+        return songList;
+    }
+
+    Song songToParse;
+
+    @Override
+    public void startElement(final String uri, final String localName, final String qName, final Attributes attributes) {
+        elementTag = qName;
+
+        if (COLUMN_HEADER_RECORD.equals(qName)) {
+            building = true;
+            songToParse = new Song();
+        }
+    }
+
+    @Override
+    public void endElement(final String uri, final String localName, final String qName) {
+        if (COLUMN_HEADER_RECORD.equals(qName)) {
+            building = false;
+            songList.add(songToParse);
+        }
+
+        xml.setLength(0);
+    }
+
+    @Override
+    public void characters(final char[] ch, final int start, final int length) {
+        if (building) {
+            String value = new String(ch, start, length);
+            if (new String(ch, start, length).trim().length() == 0) {
+                return;
+            }
+
+            if (COLUMN_HEADER_ARTIST.equals(elementTag) || COLUMN_HEADER_SONG.equals(elementTag)) {
+                final String xmlResult = xml.append(value).toString();
+                value = xmlResult.replace(SEPARATOR_CHARACTER_AMPERSAND, SEPARATOR_CHARACTER_AMPERSAND_XML_SAFE).trim();
+                if (COLUMN_HEADER_ARTIST.equals(elementTag)) {
+                    songToParse.setArtist(value);
+                }
+                if (COLUMN_HEADER_SONG.equals(elementTag)) {
+                    songToParse.setTitle(value);
+                }
+            }
+            else if (elementTag.startsWith(INFO_COLUMN_MARKER)) {
+                songToParse.addInfoToMap(elementTag, value);
+            }
+            else {
+                songToParse.addPositionToMap(elementTag, Integer.parseInt(value));
+            }
+        }
+    }
+}
--- src/main/java/parser/XMLParser.java
+++ src/main/java/parser/XMLParser.java
@@ -6,94 +6,41 @@ import javax.xml.parsers.SAXParserFactory;
 
 import dto.Song;
 import dto.SongList;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
 public class XMLParser {
 
-    private static final String INFO_COLUMN_MARKER = "ADD-";
-    private final StringBuilder xml = new StringBuilder();
-    private boolean building = false;
     private SongList songList = new SongList();
-    private String elementTag;
 
     public SongList parseXML(String file, final SongList list) {
 
         songList = list;
 
+        final SAXParserFactory factory = SAXParserFactory.newDefaultInstance();
         try {
-
-            final SAXParserFactory factory = SAXParserFactory.newInstance();
             final SAXParser saxParser = factory.newSAXParser();
-
-            final DefaultHandler handler = new DefaultHandler() {
-
-                private Song song;
-
-                public void startElement(final String uri, final String localName, final String qName, final Attributes attributes) {
-                    elementTag = qName;
-
-                    if ("record".equals(qName)) {
-                        building = true;
-                        song = new Song();
-                    }
-                }
-
-                public void endElement(final String uri, final String localName, final String qName) {
-                    if ("record".equals(qName)) {
-                        building = false;
-                        addToArrayList(song);
-                    }
-
-                    xml.setLength(0);
-                }
-
-                public void characters(final char[] ch, final int start, final int length) {
-                    if (building) {
-                        String value = new String(ch, start, length);
-                        if (new String(ch, start, length).trim().length() == 0) return; // ignore white space
-
-                        if ("Artiest".equals(elementTag) || "Nummer".equals(elementTag)) {
-                            final String xmlResult = xml.append(value).toString();
-                            value = xmlResult.replace("&", "&amp;").trim();
-                            if ("Artiest".equals(elementTag)) {
-                                song.setArtist(value);
-                            }
-                            if ("Nummer".equals(elementTag)) {
-                                song.setTitle(value);
-                            }
-                        }
-                        else if (elementTag.startsWith(INFO_COLUMN_MARKER)) {
-                            song.addInfoToMap(elementTag, value);
-                        }
-                        else {
-                            song.addPositionToMap(elementTag, Integer.parseInt(value));
-                        }
-                    }
-                }
-
-            };
+            final XMLHandler handler = new XMLHandler();
 
             if (!file.endsWith(".xml")) {
                 file = file + ".xml";
             }
             saxParser.parse(file, handler);
 
+            final SongList listFromParse = handler.getSongList();
+            for (final Song songFromList : listFromParse) {
+                addToArrayList(songFromList);
+            }
             System.out.println("Succesfully merged/initialized " + file);
-
         } catch (final FileNotFoundException e) {
             System.out.println("File not found, try again");
         } catch (final ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
         }
 
-        return songList;
-
+        return list;
     }
 
     private void addToArrayList(final Song songToAdd) {
